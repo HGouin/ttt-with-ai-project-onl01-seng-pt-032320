807ad320884359ffbf8610bd43d2f48b4eb3c0d1
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-05-18 04:10:33 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -2,19 +2,10 @@ class Board
 
   attr_accessor :cells
 
-  WIN_COMBINATIONS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [6,4,2],
-  [0,4,8]
-]
+
 
   def initialize(cells = nil)
-  @cell = cell || Array.new(9, " ")
+  @cells = cells || Array.new(9, " ")
   end
 
   def cells
