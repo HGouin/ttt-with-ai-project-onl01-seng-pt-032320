3590476fbf8988f209e8bae12e157e492ac0f558
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-05-18 03:43:21 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,31 @@
 class Board
 
+  def initialize
+  end
+
   def cells
   end
+
+  def reset!
+  end
+
+  def display
+  end
+
+  def position
+  end
+
+  def full?
+  end
+
+  def turn_count
+  end
+
+  def taken?
+  end
+
+  def valid_move?
+  end
+
+
 end
